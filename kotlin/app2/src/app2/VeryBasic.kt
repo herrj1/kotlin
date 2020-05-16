6b2004feb0b0cc3c63d8c98117c2bfc934326b1d
herrj1 <herreraj2015@fau.edu>
2020-05-15 23:09:52 -0400

    Added app2 and enhanced it

---
+++ kotlin/app2/src/app2/VeryBasic.kt
@@ -0,0 +1,6 @@
+package app1
+class VeryBasic {	
+}
+fun main(args: Array<String>){
+	val basic: VeryBasic = VeryBasic()
+}
---
+++ kotlin/app2/src/app2/abstractBakeryGood.kt
@@ -0,0 +1,39 @@
+/**
+ *
+ * author: herrj1
+ * Special notes: adapted
+ *
+ */
+
+package app2
+
+abstract class abstractBakeryGood(val flavour: String) {
+	fun eat(): String{
+		return "nom, nom, nom, nom,... delicious $flavour ${name()}"
+	}
+	
+	fun name(): String{
+		return "bakery goods "
+	}
+	
+	abstract fun name(): String
+}
+
+class Customer(val name: String){
+	fun eats(food: abstractBakeryGood){
+		println("$name was eating... ${food.eat()} on that table")
+	}
+}
+
+open class Donut(flavour: String, val topping: String): abstractBakeryGood(flavour){
+	override fun name(): String{
+		return "donut with $topping topping"
+	}
+}
+
+
+fun main(args: Array<String>){
+	val myDonut = Donut("Custard", "Powdered sugar")
+	val mario = Customer("Mario")
+	mario.eats(myDonut)
+}
